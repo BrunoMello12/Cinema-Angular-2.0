cf3bc2103dd4c4d7670b11c70811601e9559a524
Bruno <Cliente@DESKTOP-2TBK80U>
2023-10-02 12:26:37 -0300

    add favoritos na home

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Filme } from 'src/app/models/filme';
 import { FilmeService } from 'src/services/filmes.service';
+import { LocalStorageService } from 'src/services/local-storage.service';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,11 @@ import { FilmeService } from 'src/services/filmes.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit{
+  filmesFavoritos: Filme[] = [];
 
-  constructor(private filmeService: FilmeService){}
+  constructor(private localStorage: LocalStorageService){}
 
   ngOnInit(): void {
-    
+    this.filmesFavoritos = this.localStorage.carregarFavoritos()
   }
 }
