e1284e89c3c52655f209ebce1f0dd002b483bd65
Bruno <Cliente@DESKTOP-2TBK80U>
2023-10-02 12:20:16 -0300

    add favoritos

--- src/app/views/detalhes-filme/detalhes-filme.component.ts
+++ src/app/views/detalhes-filme/detalhes-filme.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { DetalhesFilme } from 'src/app/models/detalhes-filme';
+import { Filme } from 'src/app/models/filme';
 import { FilmeService } from 'src/services/filmes.service';
+import { LocalStorageService } from 'src/services/local-storage.service';
 
 @Component({
   selector: 'app-detalhes-filme',
@@ -11,18 +15,64 @@ import { FilmeService } from 'src/services/filmes.service';
 export class DetalhesFilmeComponent implements OnInit {
   filme: DetalhesFilme | undefined;
   urlSeguroTrailer: SafeResourceUrl | undefined;
+  favorito: boolean = false;
 
   constructor(private filmeService: FilmeService, 
-    private sanitazer: DomSanitizer) {}
+    private localStorageService: LocalStorageService,
+    private toastrService: ToastrService,
+    private sanitazer: DomSanitizer,
+    private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!);
+
+    if(!id) return;
+
     this.filmeService
-    .selecionarDetalhesFilme(5)
+    .selecionarDetalhesFilme(id)
     .subscribe((resposta) => {
       this.filme = resposta;
 
       this.urlSeguroTrailer =  this.sanitazer.bypassSecurityTrustResourceUrl(
         this.filme.trailers[0].sourceUrl);
     });
+  }
+
+  favoritar() {
+    if (this.favorito) {
+      this.localStorageService.desfavoritar(this.filme!.id);
+
+      this.toastrService.success(
+        'Filme removido dos favoritos com sucesso!',
+        'Sucesso'
+      );
+    } else {
+      const filmeParaFavoritar = new Filme(
+        this.filme!.id,
+        this.filme!.titulo,
+        this.filme!.imagem
+      );
+
+      this.localStorageService.favoritar(filmeParaFavoritar);
+
+      this.toastrService.success(
+        'Filme adicionado aos favoritos com sucesso!',
+        'Sucesso'
+      );
+    }
+
+    this.carregarStatusFavorito();
+  }
+
+  private carregarStatusFavorito() {
+    const favoritoEncontrado = this.localStorageService.selecionarPorId(
+      this.filme!.id
+    );
+
+    if (favoritoEncontrado) {
+      this.favorito = true;
+    } else {
+      this.favorito = false;
+    }
   }
 }
